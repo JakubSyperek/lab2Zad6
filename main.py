a5f6e6cda352ba8a86f1c247bef137af6904af41
JakubSyperek <162420@student.uwm.edu.pl>
2021-03-07 20:43:15 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+a = float(input("Podaj a:"))
+b = float(input("Podaj b:"))
+c = float(input("Podaj c:"))
+
+if a % 2 == 0:
+    print("Liczba a jest parzysta")
+else:
+    print('Liczba a jest nieparzysta')
+
+if b > c:
+    print("b jest większe od c")
+else:
+    print("c jest większe od b")
